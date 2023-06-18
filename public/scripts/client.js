1d87bec8622b876c91de2965f72ff6ca2075f0a3
Jonas Cura <jonasm.cura@gmail.com.com>
2023-06-17 21:13:11 -0600

    Add tweet box and update client.js to recieve tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,33 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+console.log('Loaded Page');
+
+const ul = document.querySelector('#tweets');
+console.log("ul:", ul);
+
+const text = document.querySelector('#tweet-text');
+console.log("text:", text);
+
+const form = document.querySelector('form');
+console.log("form:", form);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); //don't submit
+
+  // read input from field
+  const tweet = text.value;
+  console.log(tweet);
+
+  // create new li element
+  const li = document.createElement('li');
+
+  // add text to li element
+  li.textContent = tweet;
+
+  // glue li to existing ul element
+  ul.appendChild(li);
+
+  // clear out old value
+  text.value = null;
+});
