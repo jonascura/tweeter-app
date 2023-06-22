b45277d71348ba47ee15b12a52dbb04b9085df5c
Jonas Cura <jonasm.cura@gmail.com.com>
2023-06-22 17:57:35 -0600

    Add ajax get request to read data from /tweets json file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,6 +6,9 @@
 
 console.log('Loaded Page');
 
+////////////////////////////////////////////////////////////////////////////////////////////
+// Handle submit button
+////////////////////////////////////////////////////////////////////////////////////////////
 // grab elements
 const $ul = $('#tweets');
 const $text = $('#tweet-text');
@@ -21,46 +24,44 @@ $form.on('submit', (event) => {
 
   const li = `<li>${tweet}</li>`;
 
+  console.log("serial", $text.serialize());
+  $.post($text.serialize())
+
+  // $.ajax({
+  //   url: '',
+  //   type: 'GET'
+  // }).then(function(data) {
+  //   console.log("Ajax request", data);
+  // });
+
   $ul.append(li);
   $text.val(null);
   
 });
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+////////////////////////////////////////////////////////////////////////////////////////////
+// Handle GET tweets
+////////////////////////////////////////////////////////////////////////////////////////////
+const loadTweets = () => {
+  $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+      .then(function (data) {
+        console.log('GET: ', data);
+        renderTweets(data);
+      });
+};
 
+////////////////////////////////////////////////////////////////////////////////////////////
+// Handle display tweets
+////////////////////////////////////////////////////////////////////////////////////////////
 const renderTweets = function(tweets) {
-  console.log(tweets)
+  // console.log("tweets is:", tweets)
   let $div = $(".tweets-container");
   // loops through tweets
   for (let obj of tweets) { // grabs articles in data
     // calls createTweetElement for each tweet
-    console.log("obj:", obj)
+    // console.log("obj:", obj)/
     let tweetVal = createTweetElement(obj);
-    console.log("tweetVal:",tweetVal)
+    // console.log("tweetVal:",tweetVal)
     // takes return value and appends it to the tweets container
     $div.append(tweetVal);
   }  
@@ -90,8 +91,8 @@ const createTweetElement = function(tweet) {
       </div>
     </article>`
     );
-  console.log("$tweet:", $tweet);
+  // console.log("$tweet:", $tweet);
   return $tweet;
 };
 
-renderTweets(data);
+loadTweets();
